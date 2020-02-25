02135f6d6679636098dba406c571d9d6c37d8eba
Edward Tymoshuk <eduard.tymoshuk@gmail.com>
2020-02-25 20:43:30 +0100

    -m Joi library

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
 
+dotenv.config();
+
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
 
 const caseRoute = require('./routes/cases');
 const caseElementRoute = require('./routes/caseElement');
 const authRoute = require('./routes/auth');
-
-)
-
+   
 
 app.use('/cases', caseRoute);
 app.use('/caseElement', caseElementRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.post('/login', (req, res) => {
-    res.send('Register!')
+//validation
+const Joi = require('@hapi/joi');
+
+const schema = {
+    name: Joi.string().min(6).required(),
+    
+}
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch(err) {
+        res.status(400).send(err);
+    }
 })
-
 module.exports = router;
