f9672c57d6a40cbb55e0c0c95279faa3433529b2
Edward Tymoshuk <eduard.tymoshuk@gmail.com>
2020-02-26 20:11:18 +0100

    auth/register/validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,27 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const {registerValidation} = require('../validation');
 
-//validation
-const Joi = require('@hapi/joi');
-
-const schema = {
-    name: Joi.string().min(6).required(),
-    
-}
 
 router.post('/register', async (req, res) => {
+    //validation
+    const {error} = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //checking if the user is already exists in the db
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.status(400).send('Email is already exists');
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create a new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const savedUser = await user.save();
